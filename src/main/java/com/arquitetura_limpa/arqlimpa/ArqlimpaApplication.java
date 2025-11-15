1de906cddab44d009e2b5a71b9c95d4b6cffd062
Guilherme <guilherme.devsoft@gmail.com>
2025-11-15 02:07:52 -0300

    projeto criado

---
+++ src/main/java/com/arquitetura_limpa/arqlimpa/ArqlimpaApplication.java
@@ -0,0 +1,13 @@
+package com.arquitetura_limpa.arqlimpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqlimpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqlimpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitetura_limpa/arqlimpa/ArqlimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitetura_limpa.arqlimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqlimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
