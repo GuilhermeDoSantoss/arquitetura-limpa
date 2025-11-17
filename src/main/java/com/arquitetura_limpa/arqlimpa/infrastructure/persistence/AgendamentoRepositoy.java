9435d36e7e173d3f7fc72f880dc281aa77fe73a5
Guilherme <guilherme.devsoft@gmail.com>
2025-11-17 15:50:49 -0300

    criado o AgendamentoaRepository

---
+++ src/main/java/com/arquitetura_limpa/arqlimpa/infrastructure/persistence/AgendamentoRepositoy.java
@@ -0,0 +1,26 @@
+package com.arquitetura_limpa.arqlimpa.infrastructure.persistence;
+
+import com.arquitetura_limpa.arqlimpa.core.entities.Agendamento;
+import com.arquitetura_limpa.arqlimpa.core.enums.StatusAgendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface AgendamentoReposito extends JpaRepository<Agendamento, Long> {
+
+    @Query("""  
+            SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END
+            FROM AgendamentoEntity a
+            WHERE a.usuario = :usuario
+            AND  a.status = StatusAgendamento.AGENDADO
+            AND (a.dataInicio < :fim AND a.dataFim  > :inicio)
+            AND (:ignoreId is NULL OR a.id <> ignoreId)
+    """)
+
+    boolean existsConflito(@Param("usuario") String usuario,
+                           @Param("inicio")LocalDateTime inicio,
+                           @Param("fim") LocalDateTime fim,
+                           @Param("ignoreId") Long ignoreId);
+}
