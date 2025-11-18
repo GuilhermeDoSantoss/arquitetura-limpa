83df090be98014b4bba7b4baa468814d1e9c4512
Guilherme <guilherme.devsoft@gmail.com>
2025-11-17 22:33:54 -0300

    terminei o mapper, criando o updaterequest

---
+++ src/main/java/com/arquitetura_limpa/arqlimpa/infrastructure/mapper/AgendamentoUpadateRequestMapper.java
@@ -0,0 +1,51 @@
+package com.arquitetura_limpa.arqlimpa.infrastructure.mapper;
+
+import com.arquitetura_limpa.arqlimpa.core.entities.Agendamento;
+import com.arquitetura_limpa.arqlimpa.core.enums.StatusAgendamento;
+import com.arquitetura_limpa.arqlimpa.infrastructure.dto.AgendamentoUpdateRequest;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class AgendamentoUpadateRequestMapper {
+
+    public AgendamentoUpdateRequest toDto(Agendamento agendamento){
+        return new AgendamentoUpdateRequest(
+                agendamento.titulo(),
+                agendamento.descricao(),
+                agendamento.dataInicio(),
+                agendamento.dataFim()
+
+        );
+    }
+
+    public Agendamento merge(Agendamento agendamentoExistente, AgendamentoUpdateRequest req){
+        return new Agendamento(
+                agendamentoExistente.id(),
+                req.titulo() != null ? req.titulo() : agendamentoExistente.titulo(),
+                req.descricao() != null ? req.descricao() : agendamentoExistente.descricao(),
+                req.dataInicio() != null ? req.dataInicio() : agendamentoExistente.dataInicio(),
+                req.dataFim() != null ? req.dataFim() : agendamentoExistente.dataFim(),
+                agendamentoExistente.status(),
+                agendamentoExistente.usuario(),
+                agendamentoExistente.criadoEm(),
+                LocalDateTime.now()
+        );
+    }
+
+    public Agendamento toEntity(AgendamentoUpdateRequest agendamentoUpdateRequest){
+        return new Agendamento(
+                null,
+                agendamentoUpdateRequest.titulo(),
+                agendamentoUpdateRequest.descricao(),
+                agendamentoUpdateRequest.dataInicio(),
+                agendamentoUpdateRequest.dataFim(),
+                StatusAgendamento.AGENDADO,
+                null,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+}
